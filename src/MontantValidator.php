54e47c4ab86d950dadd9fffd8cde9a148084e865
Eriam Schaffter <eriam@mediavirtuel.com>
2023-09-28 19:58:53 +0200

    Initial commit

---
+++ src/MontantValidator.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Eriam\TestMontantFrais;
+
+class MontantValidator
+{
+    public static function validerMontant($montant)
+    {
+        return is_numeric($montant) && $montant >= 0.01 && $montant <= 200;
+    }
+}
+
